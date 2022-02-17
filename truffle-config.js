c9787bb6d0d74596feed9d68bb11c051c5b89275
RsTs23 <43611813+RsTs23@users.noreply.github.com>
2022-02-17 14:32:58 +0100

    Initialized project

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  networks: {
+      development: {
+          host: "localhost",
+          port: 8545,
+          network_id: "*", // Match any network id
+          skipDryRun: true,
+          production: true,
+          gasPrice: 128
+      }
+  },
+  compilers: {
+    solc: {
+      version: "0.7.5"
+    }
+  }
+};
