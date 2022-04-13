201f2fe98d7a91096e696f63b4ac8bfbbb91024e
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-04-13 16:25:49 +0100

    more brownie directives

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,2 +1,12 @@
+from brownie import accounts
+
+def deploy():
+    account = accounts[0]#for local ganache
+    #To import a wallet you need to do on the terminal:
+    #1)brownie accounts new <name of the account>
+    #2)Enter "0x"+ the private key you wish to import
+    #3)Enter a password of your choosing to encrypt the account
+    print(account)
+
 def main():
-    print("hello!")
+    deploy()
