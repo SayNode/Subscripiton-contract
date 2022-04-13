c13d046650728de85d48e80ea194a7f8a6f41935
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-04-13 16:40:31 +0100

    MORE BROWNIE

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,15 @@
 from brownie import accounts
 
 def deploy():
-    account = accounts[0]#for local ganache
+    #For local ganache
+    #account = accounts[0] 
+
     #To import a wallet you need to do on the terminal:
     #1)brownie accounts new <name of the account>
     #2)Enter "0x"+ the private key you wish to import
     #3)Enter a password of your choosing to encrypt the account
+    
+    account = accounts.load("SAYNODE")
     print(account)
 
 def main():
