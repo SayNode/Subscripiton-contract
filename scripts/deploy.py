f3374b32590f7e9c861d64c0187a5f9c6addb0e3
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-04-19 15:06:37 +0100

    deploy.py divided into functions to better testing and backend

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -30,6 +30,57 @@ def deploy():
     time.sleep(1)#avoids known Brownie error "web3 is not connected"
     return dataset_factory, dohrnii_token_contrat
 
+#Testing createDS() from DataSetFactory.sol
+def createDS(DHN, DSF, ds_creator_account):
+    print("------------------Creating a DS------------------")
+    DHN.approve(DSF,300, {"from": ds_creator_account}) #creator approves that the DSF contract 
+                                                       #can send tokens to the DS contract (amount = stakeAmount)
+
+    DSF.createDS("Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
+                 "Games","Tetris statistics and data", 10, 20, 2, {"from": ds_creator_account}) #DS created
+  
+    
+#Accesing the creator DS contract
+def getDSbyName(DSF, ds_subscriber_account):
+    print("------------------Get DataSet address by name------------------")
+    DS_address = DSF.nameToSC("Tetris",{"from": ds_subscriber_account}) #get the address of DS according to its name
+    DS = DataSet.at(DS_address) #instantiate the DS
+
+    time.sleep(1) #avoids known Brownie error "web3 is not connected"
+    print("Staked amount(correct if it is 20): " + str(DS.stakeAmount())) #see if the creator staking in the DS worked
+    return DS
+
+#Subscribing to a DS
+def subToDS(DHN,DSF, ds_subscriber_account):
+    DS = getDSbyName(DSF,ds_subscriber_account)
+    print("------------------Subbing------------------")
+    DHN.approve(DS,300, {"from": ds_subscriber_account}) #sub1 approves that the DSF contract 
+                                                          #can send tokens to the DS contract (amount = stakeAmount)
+
+    DS.subscribeToDS(0, {"from": ds_subscriber_account}) #sub1 subscribes to the "Tetris" DS
+
+    time.sleep(1) #avoids known Brownie error "web3 is not connected"
+    print("Subscriber info: ") # see info of sub1
+    print("     Price paid: "+str(DS.addressToSub(ds_subscriber_account)[0])) # see info of sub1
+    print("     Subscription Period: "+str(DS.addressToSub(ds_subscriber_account)[1])) # see info of sub1
+    print("     Subscribed at blocktimestamp: "+str(DS.addressToSub(ds_subscriber_account)[2])) # see info of sub1
+
+ #Withdraw funds Case1: booth subs have ended their sub time and the creator can withdraw
+def withdrawFunds(DHN,DSF,DS, ds_creator_account):
+    DS = getDSbyName(DSF,ds_creator_account)
+    print("------------------Withdraw Funds------------------")
+    print("Creator Balance Before Withdraw: "+str(DHN.balanceOf(ds_creator_account)))#sould be 10 (starts with 30 and stakes 20)
+    print("Sub count Before Withdraw: "+str(DS.numberOfCurrentlySubbed()) )#should be 2
+    print("Contract balance Before Withdraw: "+str(DS.getContractBalance()) )#should be 40 (20 staked by the creator+10 for each sub)
+
+    chain.sleep(10)
+    DS.withdrawFunds({"from": ds_creator_account})
+
+    time.sleep(1) #avoids known Brownie error "web3 is not connected"
+    print("Creator Balance After withdraw: " + str(DHN.balanceOf(ds_creator_account)))#should be 30 (10 he had + 10 from each sub)
+    print("Contract balance After withdraw: "+str(DS.getContractBalance()))#should bonly be the staked 20
+
+  
 def main():
 
     #Get the DataSetFactory.sol instance after deployment and the account used
@@ -46,63 +97,15 @@ def main():
     #Fund accounts
     DHN.transfer(ds_creator_account, 30, {"from": dohrnii_account}) #fund the creator
     DHN.transfer(ds_subscriber_account1, 30, {"from": dohrnii_account}) #fund sub1
-    DHN.transfer(ds_subscriber_account2, 30, {"from": dohrnii_account}) #fund sub2
+    DHN.transfer(ds_subscriber_account2, 30, {"from": dohrnii_account}) #fund sub2  
 
-#Testing createDS() from DataSetFactory.sol
-    print("------------------Creating a DS------------------")
-    DHN.approve(DSF,300, {"from": ds_creator_account}) #creator approves that the DSF contract 
-                                                       #can send tokens to the DS contract (amount = stakeAmount)
+    #Create a DS and instantiate it
+    DS = createDS(DHN, DSF, ds_creator_account)
 
-    DS = DSF.createDS("Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
-                 "Games","Tetris statistics and data", 10, 20, 2, {"from": ds_creator_account}) #DS created
-  
-    
-#Accesing the creator DS contract
-    print("------------------Get DataSet address by name------------------")
-    DS_address = DSF.nameToSC("Tetris",{"from": ds_subscriber_account1}) #get the address of DS according to its name
-    DS = DataSet.at(DS_address) #instantiate the DS
+    #Sub1
+    subToDS(DHN, DSF, ds_subscriber_account1)    
+    #Sub2
+    subToDS(DHN, DSF, ds_subscriber_account2)
 
-    time.sleep(1) #avoids known Brownie error "web3 is not connected"
-    print("Staked amount(correct if it is 20): " + str(DS.stakeAmount())) #see if the creator staking in the DS worked
-
-#Subscribing to a DS
-    print("------------------First Sub------------------")
-    DHN.approve(DS,300, {"from": ds_subscriber_account1}) #sub1 approves that the DSF contract 
-                                                          #can send tokens to the DS contract (amount = stakeAmount)
-
-    DS.subscribeToDS(0, {"from": ds_subscriber_account1}) #sub1 subscribes to the "Tetris" DS
-
-    time.sleep(1) #avoids known Brownie error "web3 is not connected"
-    print("Subscriber info: ") # see info of sub1
-    print("     Price paid: "+str(DS.addressToSub(ds_subscriber_account1)[0])) # see info of sub1
-    print("     Subscription Period: "+str(DS.addressToSub(ds_subscriber_account1)[1])) # see info of sub1
-    print("     Subscribed at blocktimestamp: "+str(DS.addressToSub(ds_subscriber_account1)[2])) # see info of sub1
-
-#Subscribing to a DS
-    print("------------------Second Sub------------------")
-    DHN.approve(DS,300, {"from": ds_subscriber_account2}) #sub1 approves that the DSF contract 
-                                                          #can send tokens to the DS contract (amount = stakeAmount)
-
-    DS.subscribeToDS(0, {"from": ds_subscriber_account2}) #sub1 subscribes to the "Tetris" DS
-
-    time.sleep(1) #avoids known Brownie error "web3 is not connected"
-    print("Subscriber info: ") # see info of sub1
-    print("     Price paid: "+str(DS.addressToSub(ds_subscriber_account2)[0])) # see info of sub1
-    print("     Subscription Period: "+str(DS.addressToSub(ds_subscriber_account2)[1])) # see info of sub1
-    print("     Subscribed at blocktimestamp: "+str(DS.addressToSub(ds_subscriber_account2)[2])) # see info of sub1
-
- #Withdraw funds Case1: booth subs have ended their sub time and the creator can withdraw
-    print("------------------Withdraw Funds------------------")
-    print("Creator Balance Before Withdraw: "+str(DHN.balanceOf(ds_creator_account)))#sould be 10 (starts with 30 and stakes 20)
-    print("Sub count Before Withdraw: "+str(DS.numberOfCurrentlySubbed()) )#should be 2
-    print("Contract balance Before Withdraw: "+str(DS.getContractBalance()) )#should be 40 (20 staked by the creator+10 for each sub)
-
-    chain.sleep(10)
-    DS.withdrawFunds({"from": ds_creator_account})
-
-    time.sleep(1) #avoids known Brownie error "web3 is not connected"
-    print("Creator Balance After withdraw: " + str(DHN.balanceOf(ds_creator_account)))#should be 30 (10 he had + 10 from each sub)
-    print("Contract balance After withdraw: "+str(DS.getContractBalance()))#should bonly be the staked 20
-
-  
-    
+    #Withdraw funds
+    withdrawFunds(DHN, DSF, DS, ds_creator_account)
--- tests/Testing.md
+++ tests/Testing.md
@@ -3,7 +3,7 @@
 - [x] Contract creation; Use this: "Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu","Games","Tetris statistics and data", 10, 30,2 and DHN address: 0x2a90E736b550E3A7AF5cD7C18F74AADa08b7410F
 - [x] Staking;
 - [x] Basic functions in DataSet.sol;
-- [x] Buying/subbing to a dataset (addressToSub[msg.sender].subbed stays false, have to find out why);
+- [x] Buying/subbing to a dataset;
 - [x] Complex functions in DataSet.sol;
 - [x] Withdrawing money from a dataset (creator) for various time sub subscribers scenarios;
 - [ ] Deleting a dataset;
