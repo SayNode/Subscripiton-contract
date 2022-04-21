45b16529d7a70b4e295cd4e95cf61fb3c80dccfe
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-04-21 10:47:31 +0100

    further correction of decimals and more testing

--- tests/test_CreateDS.py
+++ tests/test_CreateDS.py
@@ -4,6 +4,7 @@ import scripts.deploy as deployer
 #Test the creation of a new DataSet
 
 def testCreateDS():
+    dec_fit = 10**18
     #Get the DataSetFactory.sol instance after deployment and the account used
     (DSF,DHN)=deployer.deploy()
 
@@ -14,31 +15,31 @@ def testCreateDS():
     random_account1 = accounts[3]#just to call a DS by its name
 
     #Fund accounts
-    DHN.transfer(ds_creator_account1, 30, {"from": dohrnii_account}) #fund the creator
-    DHN.transfer(ds_creator_account2, 30, {"from": dohrnii_account}) #fund the creator  
+    DHN.transfer(ds_creator_account1, 30*dec_fit, {"from": dohrnii_account}) #fund the creator
+    DHN.transfer(ds_creator_account2, 30*dec_fit, {"from": dohrnii_account}) #fund the creator  
 
     #Create a DS and instantiate it
-    deployer.createDS(DHN, DSF, ds_creator_account1,"Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
-                 "Games","Tetris statistics and data", 10, 3600, 2)
+    deployer.createDS(dec_fit,DHN, DSF, ds_creator_account1,"Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
+                 "Games","Tetris statistics and data", 10*dec_fit, 3600, 2*dec_fit)
 
-    DS_instance1 = deployer.getDSbyName(DSF, random_account1, "Tetris")
+    DS_instance1 = deployer.getDSbyName(dec_fit, DSF, random_account1, "Tetris")
     
     #Create a DS and instantiate it
-    deployer.createDS(DHN, DSF, ds_creator_account2,"Desserts", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
-                 "Food","Some dessert recipes", 5, 3600, 2)
+    deployer.createDS(dec_fit, DHN, DSF, ds_creator_account2,"Desserts", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
+                 "Food","Some dessert recipes", 5*dec_fit, 3600, 2*dec_fit)
           
-    DS_instance2 = deployer.getDSbyName(DSF, random_account1, "Desserts")
+    DS_instance2 = deployer.getDSbyName(dec_fit, DSF, random_account1, "Desserts")
 
     #Assertion: DS creation
     expected1 = ("Tetris", 
                 "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
                  "Games",
                  "Tetris statistics and data",  
-                 20,
-                 20,
-                 20,
-                 2,
-                 10,
+                 20*dec_fit,
+                 20*dec_fit,
+                 20*dec_fit,
+                 2*dec_fit,
+                 10*dec_fit,
                  0, 
                  3600,
                  0,
@@ -62,11 +63,11 @@ def testCreateDS():
                 "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
                  "Food",
                  "Some dessert recipes",  
-                 20,
-                 20,
-                 20,
-                 2,
-                 5,
+                 20*dec_fit,
+                 20*dec_fit,
+                 20*dec_fit,
+                 2*dec_fit,
+                 5*dec_fit,
                  0, 
                  3600,
                  0,
--- tests/test_SubToDS.py
+++ tests/test_SubToDS.py
@@ -8,53 +8,55 @@ import scripts.deploy as deployer
                     # arrays of structs (deposits, subscribres)
 
 def testCreateDS():
+    dec_fit = 10**18
+
     #Get the DataSetFactory.sol instance after deployment and the account used
     (DSF,DHN)=deployer.deploy()
 
     #Define accounts
     dohrnii_account = accounts[0] #mints the DHN tokens
     ds_creator_account1 = accounts[1] #creates a nem Data Set callled "Tetris"
-    ds_creator_account2 = accounts[2] #creates a nem Data Set callled "Tetris"
+    ds_creator_account2 = accounts[2] #creates a nem Data Set callled "Desserts"
     ds_subscriber_account1 = accounts[3] #will subscribe to the "Tetris" dataset with a 1s sub time
     ds_subscriber_account2 = accounts[4] #will also subscribe to the "Tetris" dataset with a 1s sub time
-    ds_subscriber_account3 = accounts[5] #will subscribe to the "Tetris" dataset with a 1day sub time
-    ds_subscriber_account4 = accounts[6] #will also subscribe to the "Tetris" dataset with a 30 days sub time
+    ds_subscriber_account3 = accounts[5] #will subscribe to the "Desserts" dataset with a 1day sub time
+    ds_subscriber_account4 = accounts[6] #will also subscribe to the "Desserts" dataset with a 30 days sub time
 
     #Fund accounts
-    DHN.transfer(ds_creator_account1, 30, {"from": dohrnii_account}) #fund the creator
-    DHN.transfer(ds_creator_account2, 30, {"from": dohrnii_account}) #fund the creator
-    DHN.transfer(ds_subscriber_account1, 30, {"from": dohrnii_account}) #fund sub1
-    DHN.transfer(ds_subscriber_account2, 30, {"from": dohrnii_account}) #fund sub2
-    DHN.transfer(ds_subscriber_account3, 30, {"from": dohrnii_account}) #fund sub3
-    DHN.transfer(ds_subscriber_account4, 30, {"from": dohrnii_account}) #fund sub4    
+    DHN.transfer(ds_creator_account1, 30*dec_fit, {"from": dohrnii_account}) #fund the creator
+    DHN.transfer(ds_creator_account2, 30*dec_fit, {"from": dohrnii_account}) #fund the creator
+    DHN.transfer(ds_subscriber_account1, 30*dec_fit, {"from": dohrnii_account}) #fund sub1
+    DHN.transfer(ds_subscriber_account2, 30*dec_fit, {"from": dohrnii_account}) #fund sub2
+    DHN.transfer(ds_subscriber_account3, 30*dec_fit, {"from": dohrnii_account}) #fund sub3
+    DHN.transfer(ds_subscriber_account4, 30*dec_fit, {"from": dohrnii_account}) #fund sub4    
 
     #Create a DS and instantiate it
-    deployer.createDS(DHN, DSF, ds_creator_account1,"Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
-                 "Games","Tetris statistics and data", 10, 3600, 2)
+    deployer.createDS(dec_fit, DHN, DSF, ds_creator_account1,"Tetris", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
+                 "Games","Tetris statistics and data", 10*dec_fit, 3600, 2*dec_fit)
 
-    DS_instance1 = deployer.getDSbyName(DSF, ds_subscriber_account1, "Tetris")
+    DS_instance1 = deployer.getDSbyName(dec_fit, DSF, ds_subscriber_account1, "Tetris")
     
     #Create a DS and instantiate it
-    deployer.createDS(DHN, DSF, ds_creator_account2,"Desserts", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
-                 "Food","Some dessert recipes", 5, 3600, 2)
+    deployer.createDS(dec_fit, DHN, DSF, ds_creator_account2,"Desserts", "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu",
+                 "Food","Some dessert recipes", 5*dec_fit, 3600, 2*dec_fit)
           
-    DS_instance2 = deployer.getDSbyName(DSF, ds_subscriber_account2, "Desserts")
+    DS_instance2 = deployer.getDSbyName(dec_fit, DSF, ds_subscriber_account2, "Desserts")
 
     #Sub1
-    deployer.subToDS(DHN, DSF, ds_subscriber_account1, "Tetris", 0)    
+    deployer.subToDS(dec_fit, DHN, DSF, ds_subscriber_account1, "Tetris", 0)    
     #Sub2
-    deployer.subToDS(DHN, DSF, ds_subscriber_account2, "Tetris", 0)
+    deployer.subToDS(dec_fit, DHN, DSF, ds_subscriber_account2, "Tetris", 0)
     #Sub3
-    deployer.subToDS(DHN, DSF, ds_subscriber_account3, "Desserts", 1)    
+    deployer.subToDS(dec_fit, DHN, DSF, ds_subscriber_account3, "Desserts", 1)    
     #Sub4
-    deployer.subToDS(DHN, DSF, ds_subscriber_account4, "Desserts", 2)
+    deployer.subToDS(dec_fit, DHN, DSF, ds_subscriber_account4, "Desserts", 2)
 
 #Assertion: DS creation alterations
     
-    assert (20+10*2, 2) == (DHN.balanceOf(DS_instance1),#contract balance changes because of 2 subs
+    assert ((20+10*2)*dec_fit, 2) == (DHN.balanceOf(DS_instance1),#contract balance changes because of 2 subs
                             DS_instance1.subCount())#subcount increases by 2
     
-    assert (20+2*5,2) == (DHN.balanceOf(DS_instance2),#contract balance changes because of 2 subs
+    assert ((20+2*5)*dec_fit, 2) == (DHN.balanceOf(DS_instance2),#contract balance changes because of 2 subs
                         DS_instance2.subCount())#subcount increases by 2
     
 #Assertion: Sub info
@@ -62,12 +64,12 @@ def testCreateDS():
     #Subscribed to "Tetris"
     info1 = DS_instance1.addressToSub(ds_subscriber_account1)
     #Price paid, subscription time, Is this person currently subbed?
-    assert (10, 1, True) == (info1[0], info1[1], info1[3])
+    assert (10*dec_fit, 1, True) == (info1[0], info1[1], info1[3])
 
     #Subscribed to "Tetris"
     info2 = DS_instance1.addressToSub(ds_subscriber_account2)
     #Price paid, subscription time, Is this person currently subbed?
-    assert (10, 1, True) == (info2[0], info2[1], info2[3])
+    assert (10*dec_fit, 1, True) == (info2[0], info2[1], info2[3])
 
     #Not subscribed to "Tetris"
     info3 = DS_instance1.addressToSub(ds_subscriber_account3)
@@ -77,12 +79,12 @@ def testCreateDS():
     #Subscribed to "Desserts"
     info4 = DS_instance2.addressToSub(ds_subscriber_account3)
     #Price paid, subscription time, Is this person currently subbed?
-    assert (5, 24*3600, True) == (info4[0], info4[1], info4[3])
+    assert (5*dec_fit, 24*3600, True) == (info4[0], info4[1], info4[3])
 
     #Subscribed to "Desserts"
     info5 = DS_instance2.addressToSub(ds_subscriber_account4)
     #Price paid, subscription time, Is this person currently subbed?
-    assert (5, 30*24*3600, True) == (info5[0], info5[1], info5[3])
+    assert (5*dec_fit, 30*24*3600, True) == (info5[0], info5[1], info5[3])
 
     #Not subscribed to "Desserts"
     info6 = DS_instance2.addressToSub(ds_subscriber_account1)
@@ -90,6 +92,11 @@ def testCreateDS():
     assert (0, 0, False) == (info6[0], info6[1], info6[3])
 
 #Assertion: Deposits
+
+    #Subscribed to "Tetris"
+    info1 = DS_instance1.deposits(0)
+    #Price paid, subscription time, Is this person currently subbed?
+    assert (ds_subscriber_account1, 10*dec_fit) == (info1[0], info1[1])
 
 #Assertion: Mappings
 
