34c925f80f7ce0e2192c2fd253c8350d025acc98
paulamstutz <paula@saynode.ch>
2022-04-29 10:00:52 +0200

    download.py

---
+++ flask_api/download.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/download")
+def download_files():
+    path = "static/uploads/screenshot.png"
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# to download a file we run: http://127.0.0.1:5000/download and the file will be downloaded
+
+# to do: find a way to download specific files that are uploaded from the the ulpoad.py file without needed to write a function for each
