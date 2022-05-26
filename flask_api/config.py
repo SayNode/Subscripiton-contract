a09491dfb39c19ff2be3ffba617db971e948b607
paulamstutz <paula@saynode.ch>
2022-05-26 17:12:48 +0200

    Marketplace API

---
+++ flask_api/config.py
@@ -0,0 +1,3 @@
+IPFS_CONNECT_URL = "/ip4/127.0.0.1/tcp/5001"
+IPFS_FILE_URL = "http://ip4/127.0.0.1/tcp/4001/"
+DOMAIN = "http://ipfs.io/ipfs"
--- flask_api/upload.py
+++ flask_api/upload.py
@@ -1,34 +1,22 @@
+from inspect import signature
 from msilib.schema import Class
 from flask import Flask, json, request, jsonify, render_template, redirect, url_for
-
-# from flask.ext.sqlalchemy import SQLAlchemy
+import ipfshttpclient
+from flask_cors import CORS
 from flask_restful import reqparse, abort, Api, Resource
 import os
 from os.path import join, dirname, realpath
 import urllib.request
-import werkzeug
 from werkzeug.utils import secure_filename
 import os
-
-"""
-with open(join(dirname(realpath(__file__)), "/entrust_2048_ca.cer"), "r") as file:
-    data = file.read()
-x509 = crypto.load_certificate(crypto.FILETYPE_PEM, data)
-p12 = crypto.PKCS12()
-p12.set_certificate(x509)
-
-context = SSL.Context(SSL.TLSv1_2_METHOD)
-context.use_certificate("entrust_2048_ca.crt")
-context.use_privatekey("server.key")
-
-"""
+import logging
+from flask_script import Manager
+import encryption
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), "static/uploads/..")
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+CORS(app)
+app.config.from_pyfile("config.py")
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
-
 ALLOWED_EXTENSIONS = set([".xlsx", "pdf", "pptx"])
 
 
@@ -37,8 +25,12 @@ def allowed_file(filename):
 
 
 Files = {}
+manager = Manager(app)
 
-api = Api(app)
+console = logging.StreamHandler()
+root = logging.getLogger("")
+root.addHandler(console)
+api = Api(app, catch_all_404s=True)
 
 
 class Main(Resource):
@@ -48,44 +40,41 @@ class Main(Resource):
 
 api.add_resource(Main, "/")
 
-''' def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument(
-            "file", type=werkzeug.datastructures.FileStorage, location="files"
-        )
-        """self.reqparse.add_argument("lat", type=float, default="")
-        self.reqparse.add_argument("lon", type=float, default="")"""
-        super(uploadPhoto, self).__init__()'''
 
-
-class uploadImage(Resource):
+class uploadFile(Resource):
     def post(self):
-        file = request.files["file"]
-        if file and allowed_file(file.filename):
-            # From flask uploading tutorial
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return redirect(url_for("uploadimage", filename=filename))
-        else:
-            # return error
-            return {"False"}
+        try:
+            file = request.files["file"]
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                logging.info("file name: {}".format(file.filename), {"app": "api"})
+                client = ipfshttpclient.connect(app.config["IPFS_CONNECT_URL"])
+                res = client.add(file)
+                logging.info("upload res: {}".format(res), {"app": "upload"})
+                url = str(app.config["DOMAIN"] + "/" + str(res["Hash"]))
+                # return url
+                encrypted_url = str(encryption.encrypt(url, encryption.pubKey))
+                return encrypted_url
+        except Exception as e:
+            logging.exception("Upload Error! exception:{}".format(str(e)))
+            return "Upload Error! \n", 503
 
 
-api.add_resource(uploadImage, "/upload")
+api.add_resource(uploadFile, "/upload")
 
 
 if __name__ == "__main__":
-    app.run(ssl_context=("cert.pem", "key.pem"))
+    # app.run(ssl_context=("cert.pem", "key.pem"))
     # app.run(debug=True, ssl_context=context, port=4000)
-    # app.run(debug=True)
+    app.run(debug=True)
 
-# To run it download a test rest api app, I used the extenstion Advanced Rest Client.
+# To run it download a test rest api app, I used Advanced Rest Client.
 # Method: post
 # Request URL:  http://127.0.0.1:5000/upload
 # Header: Header name: Content-Type
 #         Header value: multipart/form-data
 # Body:
-# Field name: files[] and the Choose the file you want to upload
+# Field name: <file> and then choose the file you want to upload
 
 
 # NOW POSSIBLE FROM THE WEB INTERFACE
